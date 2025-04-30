f1f25951385731d49dad8be41f76e58da7ffd27f
Mikhail Kozlov <wmikkoz@icloud.com>
2025-04-30 14:54:14 +0500

    Add docker-compose for postgres dev db and cli to apply migrations

---
+++ cmd/pgsetup/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"embed"
+	"log/slog"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
+)
+
+//go:embed migrations/*.sql
+var embedMigrations embed.FS
+
+func main() {
+	log := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+	}))
+
+	db, err := sql.Open("pgx", os.Getenv("POSTGRES_ADDRESS"))
+	if err != nil {
+		log.Error("couldn't open postgres", "error", err)
+		return
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Error("no access to db", "error", err)
+		return
+	}
+
+	goose.SetDialect("postgresql")
+	goose.SetBaseFS(embedMigrations)
+
+	if err := goose.Up(db, "migrations"); err != nil {
+		log.Error("couldn't migrate", "error", err)
+		return
+	}
+
+	if err := goose.Version(db, "migrations"); err != nil {
+		log.Error("couldn't get db version, or version is wrong", "error", err)
+		return
+	}
+
+	slog.Info("migration done")
+}
---
+++ cmd/pgsetup/migrations/20250429153218_create_sesc_tables.sql
@@ -0,0 +1,54 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE permissions (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE roles (id UUID PRIMARY KEY, name VARCHAR(255) UNIQUE);
+
+CREATE TABLE permissions_roles (
+    permission_id UUID REFERENCES permissions (id) ON DELETE CASCADE,
+    role_id UUID REFERENCES roles (id) ON DELETE CASCADE,
+    PRIMARY KEY (permission_id, role_id)
+);
+
+CREATE TABLE departments (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    description TEXT,
+    head_user_id UUID NOT NULL
+);
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    middle_name TEXT DEFAULT '' NOT NULL,
+    picture_url TEXT,
+    suspended BOOLEAN DEFAULT false,
+    department_id UUID REFERENCES departments (id),
+    role_id UUID REFERENCES roles (id),
+    auth_id UUID NOT NULL
+);
+
+ALTER TABLE departments ADD FOREIGN KEY (head_user_id) REFERENCES users (id);
+
+CREATE TABLE users_extra_permissions (
+    user_id UUID REFERENCES users (id),
+    permission_id UUID REFERENCES permissions (id),
+    PRIMARY KEY (user_id, permission_id)
+);
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE permissions_roles,
+roles,
+permissions,
+departments,
+users,
+users_extra_permissions CASCADE;
+
+-- +goose StatementEnd
