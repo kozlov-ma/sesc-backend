b486ee49b7918e9d9c65e5d40c636ed5c31b3e0a
Ryuk124 <kostss332@gmail.com>
2025-05-25 19:37:43 +0500

    Rebase onto main

--- db/entdb/ent/migrate/schema.go
+++ db/entdb/ent/migrate/schema.go
@@ -107,7 +107,7 @@ var (
 	// AchievementTemplatesColumns holds the columns for the "achievement_templates" table.
 	AchievementTemplatesColumns = []*schema.Column{
 		{Name: "id", Type: field.TypeUUID, Unique: true},
-		{Name: "name", Type: field.TypeString, Size: 255},
+		{Name: "name", Type: field.TypeString, Size: 600},
 		{Name: "description", Type: field.TypeString, Nullable: true, Size: 2147483647},
 		{Name: "points_limit", Type: field.TypeInt},
 		{Name: "active", Type: field.TypeBool, Default: true},
